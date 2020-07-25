603d3cecbc475ad5e2e51f52f6ceb7f874f7d053
Naaman <naaman.the.dev@gmail.com>
2020-07-25 16:34:34 +0100

    feat(app-api): Update project api with additional methods

--- app/src/api/project.api.ts
+++ app/src/api/project.api.ts
@@ -1,17 +1,50 @@
 import { HttpClient } from './http-client-base.abstract';
 import { Project } from '../models/project';
+import { AxiosPromise } from 'axios';
+
+export enum UpdateProject {
+  AddProjectMember = 'AddProjectMember',
+  RemoveProjectMember = 'RemoveProjectMember',
+}
+
+type ProjectUpdateObject<T> = {
+  update: {
+    [key: string]: T;
+  };
+};
 
 class ProjectApi extends HttpClient {
   public constructor() {
     super(process.env.REACT_APP_API_URL!);
   }
 
-  public createNewProject = (project: Project) => {
+  public createNewProject = (project: Project): AxiosPromise<Project> => {
     return this.client.post<Project>('/projects', project);
   };
 
-  public getProjects = () => {
+  public getProject = (projectId: string): AxiosPromise<Project> => {
+    return this.client.get<Project>(`/projects/${projectId}`);
+  };
+
+  public getProjects = (): AxiosPromise<Project[]> => {
     return this.client.get<Project[]>('/projects');
+  };
+
+  public updateProject<T>(
+    projectId: string,
+    updateType: UpdateProject,
+    updateValue: T,
+  ): AxiosPromise<Project> {
+    const updateObject: ProjectUpdateObject<T> = {
+      update: {
+        [updateType]: updateValue,
+      },
+    };
+    return this.client.put(`/projects/${projectId}`, updateObject);
+  }
+
+  public deleteProject = (projectId: string): AxiosPromise<void> => {
+    return this.client.delete(`/projects/${projectId}`);
   };
 }
 
